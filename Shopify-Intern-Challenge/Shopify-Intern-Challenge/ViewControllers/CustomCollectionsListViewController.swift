9383e3d46e6b191d71c7542f828a1e9ed6fd386e
Michael Ding <m.ding5692@gmail.com>
2019-01-08 23:23:43 -0500

    Finalizes Custom Collections List VC for now

--- Shopify-Intern-Challenge/Shopify-Intern-Challenge/ViewControllers/CustomCollectionsListViewController.swift
+++ Shopify-Intern-Challenge/Shopify-Intern-Challenge/ViewControllers/CustomCollectionsListViewController.swift
@@ -31,6 +31,20 @@ extension CustomCollectionsListViewController: UITableViewDelegate, UITableViewD
     fileprivate func initiateCustomCollectionsTableView() {
         customCollectionsTableView.delegate = self
         customCollectionsTableView.dataSource = self
+        
+        // Shadow and curved borders for tableView
+        let containerView = UIView(frame: customCollectionsTableView.frame)
+        containerView.backgroundColor = .clear
+        containerView.layer.shadowColor = UIColor.darkGray.cgColor
+        containerView.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        containerView.layer.shadowOpacity = 1.0
+        containerView.layer.shadowRadius = 2
+        
+        customCollectionsTableView.layer.cornerRadius = 10
+        customCollectionsTableView.layer.masksToBounds = true
+        
+        view.addSubview(containerView)
+        containerView.addSubview(customCollectionsTableView)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -44,7 +58,7 @@ extension CustomCollectionsListViewController: UITableViewDelegate, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        print(indexPath.row)
     }
     
 }
